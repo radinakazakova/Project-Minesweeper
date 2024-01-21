578293c3d6d9c498fc986f5801db6df218bde34b
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-01-21 21:27:27 +0200

    Update final-project-minesweeper.cpp

--- final-project-minesweeper.cpp
+++ final-project-minesweeper.cpp
@@ -1,3 +1,17 @@
+/**
+*
+* Solution to course project # 5
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Radina Kazakova
+* @idnumber 0MI0600282
+* @compiler VC
+*
+* <game file>
+*
+*/
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
